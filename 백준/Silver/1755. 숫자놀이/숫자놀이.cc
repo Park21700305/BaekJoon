02d32901f70ef4c73f70e1ca9be7f006eac694ce
ParkJiSung <gggg0195@gmail.com>
2022-08-01 18:14:24 +0900

    [Silver IV] Title: 숫자놀이, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/1755. 숫자놀이/숫자놀이.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(){
+    int N, M;
+    string s;
+    string nums[11] = { "zero" ,"one" ,"two" ,"three" ,"four" ,"five" ,"six" ,"seven" ,"eight" ,"nine" };
+    vector<pair<string ,int>> ans;
+	
+    cin >> M >> N;
+	
+	for (int i=M; i<=N; i++){
+		if (i >= 10)
+		{
+			s = nums[i/10] + " " + nums[i%10];
+			ans.push_back({s,i});
+		}
+		else
+		{
+			s = nums[i % 10];
+			ans.push_back({s,i});
+		}
+	}
+
+	sort(ans.begin(), ans.end());
+
+	for (int i=0; i < ans.size(); i++){
+		cout << ans[i].second << " ";
+		if (i % 10 == 9) cout << '\n';
+	}
+}
