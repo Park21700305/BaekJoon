844ef241e5acfa4c0553f7e73b237f0edb1ed3f4
ParkJiSung <gggg0195@gmail.com>
2024-01-29 14:25:16 +0900

    [Bronze V] Title: 검증수, Time: 216 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int num = scanner.nextInt();
+            sum += num * num;
+        }
+
+        int lastDigit = sum % 10;
+        System.out.println(lastDigit);
+
+        scanner.close();
+    }
+}
