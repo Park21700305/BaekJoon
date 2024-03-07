e808342d13444227434a78dc579fec23401474ee
ParkJiSung <gggg0195@gmail.com>
2024-03-07 22:30:52 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,6 +1,6 @@
 -- 코드를 작성해주세요
-select ITEM_ID, ITEM_NAME, RARITY from ITEM_INFO
-natural join ITEM_TREE
+select ITEM_ID, ITEM_NAME, RARITY from ITEM_TREE
+natural join ITEM_INFO
 where PARENT_ITEM_ID in (select ITEM_ID from ITEM_INFO
 where RARITY = 'RARE')
 order by ITEM_ID desc;
